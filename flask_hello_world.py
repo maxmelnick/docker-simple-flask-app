6cedf7cc1c2c99a1f6e9f6f59e455114281a98d6
Max Melnick <maxmelnick@gmail.com>
2016-05-31 20:41:19 -0400

    test update message

--- flask_hello_world.py
+++ flask_hello_world.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World using cherrypy!"
+    return "Hello World using cherrypy2!"
 
 if __name__ == "__main__":
     app.run()

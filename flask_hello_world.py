dd1e4cc7a92adb8eacb22566f5ddac6f489a5175
Max Melnick <maxmelnick@gmail.com>
2016-06-01 23:50:45 -0400

    updated message for google cloud test

--- flask_hello_world.py
+++ flask_hello_world.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World using cherrypy2!"
+    return "Hello World using cherrypy2 on Google Container Engine!"
 
 if __name__ == "__main__":
     app.run()
